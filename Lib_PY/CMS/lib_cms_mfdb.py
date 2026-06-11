6f003fff988f11316c5dcd7adfcc864be05ec719
Elton Boehnen <boehnenelton2024@gmail.com>
2026-06-10 19:41:23 -0700

    feat(cms): bump to v2.1.0 (backups, imports, asset optimization)

--- Lib_PY/CMS/lib_cms_mfdb.py
+++ Lib_PY/CMS/lib_cms_mfdb.py
@@ -4,7 +4,7 @@ Family:       CMS
 Jurisdiction: ["BEJSON_LIBRARIES", "PY"]
 Status:       OFFICIAL
 Author:       Elton Boehnen
-Version:      2.0.4 OFFICIAL
+Version:      2.1.0 OFFICIAL
             MFDB Version: 1.31
 Format_Creator: Elton Boehnen
 Date:         2026-05-18
